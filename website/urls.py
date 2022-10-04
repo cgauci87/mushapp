f047d862f69a0cc4b81f3c0aa16c4fc710a35404
cgauci87 <cgauci@gmail.com>
2022-10-04 15:37:36 +0000

    Add responsive navigation bar in base.html

---
+++ website/urls.py
@@ -0,0 +1,25 @@
+from . import views
+from django.urls import path
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('index.html', views.index, name='index'),
+    path('who-are-we.html', views.whoarewePage, name='whoarewe'),
+    path('know-your-mushrooms.html', views.knowyourmushroomsPage, name='knowyourmushrooms'),
+    path('forum.html', views.forumPage, name='forum'),
+    path('contact-us.html', views.ContactusPage, name='contactus'),
+    path('contact-us-admin.html', views.ContactusAdminPage, name='contactusadmin'),
+    path('login.html', views.loginPage, name='login'),
+    path('usr-registration.html', views.RegistrationPage, name='registration'),
+    path('usr-account.html', views.AccountPage, name='account'),
+    path('forgot-password.html', views.ForgotPasswordPage, name='forgotpassword'),
+    path('password-reset.html', views.PasswordResetPage, name='passwordreset'),
+    path('email-verification.html', views.EmailVerificationPage, name='emailverification'),
+    path('change-password.html', views.ChangePasswordPage, name='changepassword'),
+    path('write-a-review.html', views.WriteReviewPage, name='writereview'),
+    path('products.html', views.ProductsPage, name='products'),
+    path('add-product.html', views.AddProductPage, name='addproduct'),
+    path('edit-product.html', views.EditProductPage, name='editproduct'),
+    path('approve-reviews.html', views.ApproveReviewsPage, name='approvereviews'),
+]
---
+++ website/views.py
@@ -0,0 +1,92 @@
+from django.shortcuts import render
+from django.views import View
+
+
+def index(request):
+    """ Return homepage """
+    return render(request, 'index.html')
+
+
+def whoarewePage(request):
+    """ Who Are We page """
+    return render(request, 'who-are-we.html')
+
+
+def knowyourmushroomsPage(request):
+    """ Know Your Mushrooms page """
+    return render(request, 'know-your-mushrooms.html')
+
+
+def forumPage(request):
+    """ Forum page """
+    return render(request, 'forum.html')
+
+
+def ContactusPage(request):
+    """ Contact Us page """
+    return render(request, 'contact-us.html')
+
+
+def ContactusAdminPage(request):
+    """ Contact Us Admin page """
+    return render(request, 'contact-us-admin.html')
+
+
+def loginPage(request):
+    """ Return Login page """
+    return render(request, 'login.html')
+
+
+def RegistrationPage(request):
+    """ Return Registration page """
+    return render(request, 'usr-registration.html')
+
+
+def AccountPage(request):
+    """ Return Account page """
+    return render(request, 'usr-account.html')
+
+
+def ForgotPasswordPage(request):
+    """ Forgot Password page """
+    return render(request, 'forgot-password.html')
+
+
+def PasswordResetPage(request):
+    """ Password Reset page """
+    return render(request, 'password-reset.html')
+
+
+def EmailVerificationPage(request):
+    """ Email Verification page """
+    return render(request, 'email-verification.html')
+
+
+def ChangePasswordPage(request):
+    """ Change Password page """
+    return render(request, 'change-password.html')
+
+
+def WriteReviewPage(request):
+    """ Write Review page """
+    return render(request, 'write-a-review.html')
+
+
+def ProductsPage(request):
+    """ Products page """
+    return render(request, 'products.html')
+
+
+def AddProductPage(request):
+    """ Add product page """
+    return render(request, 'add-product.html')
+
+
+def EditProductPage(request):
+    """ Edit product page """
+    return render(request, 'edit-product.html')
+
+
+def ApproveReviewsPage(request):
+    """ Approve reviews page """
+    return render(request, 'approve-reviews.html')
