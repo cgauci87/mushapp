8caf06676b9ed133781c55ce76d2470b45a7ac35
cgauci87 <cgauci@gmail.com>
2022-10-13 21:20:36 +0000

    fixed 403 error in index.html by putting script in usr-account.html

--- static/js/script.js
+++ static/js/script.js
@@ -64,26 +64,3 @@ $(document).ready(function () {
         $("#testimonials-goldenoyster").get(0).scrollIntoView();
     });
 });
-
-// upload profile photo
-get_user_profile();
-
-function readURL(input) {
-    if (input.files && input.files[0]) {
-        var reader = new FileReader();
-        reader.onload = function (e) {
-            $("#imagePreview").css(
-                "background-image",
-                "url(" + e.target.result + ")"
-            );
-            $("#imagePreview").hide();
-            $("#imagePreview").fadeIn(650);
-        };
-        reader.readAsDataURL(input.files[0]);
-    }
-}
-$("#imageUpload").change(function () {
-    readURL(this);
-});
-
-
