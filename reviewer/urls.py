777e3f79dfe444c0c43aaa887a9a8bf7af6f2e34
cgauci87 <cgauci@gmail.com>
2022-10-17 20:22:52 +0000

    modify urlpatterns in reviewer/urls.py

--- reviewer/urls.py
+++ reviewer/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('users/', include('users.urls')),
     path('products/', include('products.urls')),
     path('admin-app/', include('admin_app.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
