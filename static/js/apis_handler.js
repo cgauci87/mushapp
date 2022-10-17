749b0a70111c90e6f609d27f5154436c8d2e91e2
cgauci87 <cgauci@gmail.com>
2022-10-17 00:16:16 +0000

    Revert "add new function upload_profile_photo - apis_handler.js"
    This reverts commit e95ac7ac2ba7ff74a0dc035a89ded2f35b68dc8b.

--- static/js/apis_handler.js
+++ static/js/apis_handler.js
@@ -738,14 +738,15 @@ function update_profile() {
     });
 }
 
-// upload profile photo
-function upload_profile_photo() {
+//convert image into base64
+function getBaseUrl() {
     var token = getCookie("token");
     var headers = {
         'Authorization': 'Token ' + token //Authentication - to verify the user login using token from cookies and provide security for the API
     };
+    var file = document.querySelector('input[type=file]')['files'][0];
     var fd = new FormData();
-    fd.append('image', $('#profile-photo')[0].files[0]);
+    fd.append('profile_image', file);
     $.ajax({
         url: userProfileUrl,
         data: fd,
