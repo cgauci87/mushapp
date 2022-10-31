5fced74d264a32d2786cc8ea283f4aebab98e604
cgauci87 <cgauci@gmail.com>
2022-10-31 13:37:23 +0000

    debug false

--- reviewer/settings.py
+++ reviewer/settings.py
@@ -31,7 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mushcommunity-app.herokuapp.com', 'localhost']
 CORS_ORIGIN_ALLOW_ALL = True
