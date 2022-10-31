287bf27854f02b2966a6787c0c4937e13a80decc
cgauci87 <cgauci@gmail.com>
2022-10-31 13:36:19 +0000

    troubleshooting staticfiles

---
+++ staticfiles/js/script.3dae096ecba3.js
@@ -0,0 +1,2 @@
+// This will show or hide the [Login/Logout/Write a Review/Pending Reviews/Products/Incoming Messages] buttons according to the user_token and user_type of the user.
+change_buttons_behaviors();
