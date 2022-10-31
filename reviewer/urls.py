e472c592f27f8c0ce3a8f9a1881c03f5efa8aa00
cgauci87 <cgauci@gmail.com>
2022-10-31 12:31:00 +0000

    revert staticfiles

--- reviewer/urls.py
+++ reviewer/urls.py
@@ -20,9 +20,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('website.urls'), name='website_urls'),
-    path('admin/', admin.site.urls),    
+    path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('products/', include('products.urls')),
     path('admin-app/', include('admin_app.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
